d5dd95b4358d848012979a51b2a666a6831e4727
Rishabh Hatgadkar <rhatgadkar@gmail.com>
2017-02-22 10:22:09 -0800

    Add run_java_test.py script and support for running java-test in screen_spawner

--- test/screen_spawner.py
+++ test/screen_spawner.py
@@ -2,11 +2,20 @@ from screenutils import Screen
 from os import system
 import sys
 
-cmd = 'python run_cpp_test.py'
-
 if len(sys.argv) <= 1 or len(sys.argv) > 2:
     print 'There must be 1 argument for number of screen sessions.'
     sys.exit()
+
+cmd = ''
+
+while True:
+    user_input = raw_input("Press '1' for cpp or '2' for java: ")
+    if user_input == '1':
+        cmd = 'python run_cpp_test.py'
+        break
+    if user_input == '2':
+        cmd = 'python run_java_test.py'
+        break
 
 #screens_list = []
 for i in range(0, int(sys.argv[1])):
