6361dab6f6849e8f9af673948897fbed2d37336e
Rishabh Hatgadkar <rhatgadkar@gmail.com>
2017-04-19 21:49:00 -0700

    Add gameOverMsg for FvtTicTacToe

--- java-client/src/tictactoe/FvtTicTacToe.java
+++ java-client/src/tictactoe/FvtTicTacToe.java
@@ -123,8 +123,7 @@ public class FvtTicTacToe implements ITicTacToe {
 
 	@Override
 	public void showGameOverDialog(String message) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(message);
 	}
 
 }
