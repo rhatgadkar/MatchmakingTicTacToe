50ba3dea770e660f690481e86abf58f732c10384
Rishabh Hatgadkar <rhatgadkar@gmail.com>
2017-04-22 00:21:02 -0700

    Rename test folder to functional-tests

--- functional-tests/run_java_test.py
+++ functional-tests/run_java_test.py
@@ -6,7 +6,6 @@ CMD = 'java -cp %s tictactoe.TicTacToe %s'
 working_dir = sys.argv[1]
 tictactoe_args = ' '.join(sys.argv[2:])
 CMD = CMD % (working_dir, tictactoe_args)
-print CMD
 while True:
     a = system(CMD)
     if a != 0:
