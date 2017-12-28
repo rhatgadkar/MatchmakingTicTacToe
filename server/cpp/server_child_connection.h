83c9664be3f83819acc6358e4864a01a21d905b2
rhatgadkar <rishabhhatgadkar@gmail.com>
2017-12-28 11:46:28 -0800

    Add implementation for ServerChildConnection

--- server/cpp/server_child_connection.h
+++ server/cpp/server_child_connection.h
@@ -8,32 +8,37 @@
 class ServerChildConnection : public ChildConnection
 {
 public:
-	ServerChildConnection(int port) : m_hostPort(port) {}
-	virtual ~ServerChildConnection() {}
+	ServerChildConnection(int port);
+	virtual ~ServerChildConnection();
 
 	// client 1
-	virtual int getClient1Port() const { return m_clientPort; }
-	virtual std::string getClient1IP() const { return m_clientIP; }
-	virtual std::string receiveFromClient1(int time) { return ""; };
-	virtual void sendToClient1(std::string text) {}
-	virtual void acceptClient1(int time = 0) {}
-	virtual void closeClient1() {}
+	virtual int getClient1Port() const { return m_clientPort1; }
+	virtual std::string getClient1IP() const { return m_clientIP1; }
+	virtual std::string receiveFromClient1(int time);
+	virtual void sendToClient1(std::string text);
+	virtual void acceptClient1(int time = 0);
+	virtual void closeClient1();
 
 	// client 2
-	virtual int getClient2Port() const { return m_clientPort; }
-	virtual std::string getClient2IP() const { return m_clientIP; }
-	virtual std::string receiveFromClient2(int time) { return ""; };
-	virtual void sendToClient2(std::string text) {}
-	virtual void acceptClient2(int time = 0) {}
-	virtual void closeClient2() {}
+	virtual int getClient2Port() const { return m_clientPort2; }
+	virtual std::string getClient2IP() const { return m_clientIP2; }
+	virtual std::string receiveFromClient2(int time);
+	virtual void sendToClient2(std::string text);
+	virtual void acceptClient2(int time = 0);
+	virtual void closeClient2();
 
 private:
 	int m_sockfd;
 	int m_hostPort;
 	struct addrinfo* m_servinfo;
-	int m_clientSockfd;
-	int m_clientPort;
-	std::string m_clientIP;
+
+	int m_clientSockfd1;
+	int m_clientPort1;
+	std::string m_clientIP1;
+
+	int m_clientSockfd2;
+	int m_clientPort2;
+	std::string m_clientIP2;
 };
 
 #endif  // SERVER_CHILD_CONNECTION_H
